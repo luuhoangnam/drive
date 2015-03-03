97baa478e8bb93f4e60998155928f2cd57ee96cc
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-04 03:07:14 +0700

    Add config

--- config/drive.php
+++ config/drive.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-
+    'location'  => 'files', // Absolutely to filesystem.disks.local.root config
+    'structure' => '{year}/{month}/{name}.{ext}', // Variables: year, month, name, ext
+    // Validation rules
+    'rules'     => [
+        'max:2048', // Kilobytes
+    ],
+    'profiles'  => [
+        'avatar' => [
+            'type'     => 'image',
+            // Steps
+            'crop'     => [100, 100, 25, 25], // width, height, x, y
+            'heighten' => [200], // height
+        ],
+    ],
 ];
